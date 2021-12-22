ba8e05e61778932baa1f8f6d69ea79f678455a48
BennyKharisma <beni.karisma@yahoo.com>
2021-12-22 15:44:54 +0700

    Migration and model done

--- database/migrations/2021_12_22_083006_create_transaction_headers_table.php
+++ database/migrations/2021_12_22_083006_create_transaction_headers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionHeadersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transaction_headers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('gender');
-            $table->rememberToken();
+            $table->unsignedInteger('userId');
+            $table->foreign('userId')->references('id')->on('users');
+            $table->datetime('dateOrder');
+            $table->integer('grandTotal');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transaction_headers');
     }
 }
---
+++ database/migrations/2021_12_22_083220_create_transaction_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('transactionHeaderId');
+            $table->foreign('transactionHeaderId')->references('id')->on('transaction_headers');
+            $table->unsignedInteger('productId');
+            $table->foreign('productId')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->integer('subTotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
